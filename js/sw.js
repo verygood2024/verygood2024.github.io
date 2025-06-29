3990b3123546412304881bbe5bc4b7ce261a9568
haizhimenhao@outlook.com <haizhimenhao@outlook.com>
2025-06-29 15:13:54 +0800

    Site updated: 2025-06-29 15:13:24

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,39 @@
-  // 注销所有现有的 Service Worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(registration => {
-        registration.unregister().then(() => {
-          console.log('Service Worker 已注销');
-        }).catch(error => {
-          console.log('注销失败:', error);
-        });
+if ('serviceWorker' in navigator) {
+  // 先注销所有已注册的旧的 Service Worker
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister().then(() => {
+        console.log('旧的 Service Worker 被注销');
+      }).catch((error) => {
+        console.log('注销旧 Service Worker 失败', error);
       });
-    });
-  }
+    }
+  });
 
-  // 清除浏览器缓存
-  if ('caches' in window) {
-    caches.keys().then(cacheNames => {
-      Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))
-        .then(() => {
-          console.log('所有缓存已清除');
-        })
-        .catch((error) => {
-          console.log('清除缓存失败:', error);
-        });
-    });
-  }
+  // 注册新的 Service Worker
+  navigator.serviceWorker.register('/service-worker.js?v=' + new Date().getTime()).then(function(registration) {
+    console.log('新的 Service Worker 已注册:', registration);
 
-  // 强制页面刷新，确保使用最新资源
-  if (navigator.serviceWorker) {
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      window.location.reload();  // 页面刷新，确保加载最新内容
-    });
-  }
+    // 激活新的 Service Worker
+    if (registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+
+    // 当新的 Service Worker 安装成功后，立即激活
+    registration.onupdatefound = () => {
+      const newWorker = registration.installing;
+
+      newWorker.onstatechange = () => {
+        if (newWorker.state === 'installed') {
+          if (navigator.serviceWorker.controller) {
+            console.log('新的 Service Worker 已安装');
+            newWorker.postMessage({ type: 'SKIP_WAITING' });
+          }
+        }
+      };
+    };
+  }).catch(function(error) {
+    console.error('Service Worker 注册失败:', error);
+  });
+}
+
